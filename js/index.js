b0862cfedc89d44070226d66910ea0b574d1775c
piotr.p <piotr.p@yggdrasilgaming.com>
2020-11-28 23:41:26 +0100

    QARANTANNA: Initial setup

---
+++ js/index.js
@@ -0,0 +1,26 @@
+import logo from "../img/logo.png";
+import * as PIXI from 'pixi.js';
+import ExactValueProvider from "./ExactValueProvider";
+
+const app = new PIXI.Application();
+
+document.body.appendChild(app.view);
+
+PIXI.Loader.shared.add('logo', logo).load((loader, resources) => {
+
+	const bunny = new PIXI.Sprite(resources.logo.texture);
+
+	bunny.x = app.renderer.width / 2;
+	bunny.y = app.renderer.height / 2;
+
+	bunny.anchor.x = 0.5;
+	bunny.anchor.y = 0.5;
+
+	app.stage.addChild(bunny);
+
+	const exactValueProvider = new ExactValueProvider();
+
+	app.ticker.add(() => {
+		bunny.rotation += exactValueProvider.giveMeOneHundredth();
+	});
+});
