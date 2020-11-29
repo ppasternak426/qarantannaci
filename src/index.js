6b4e0722b9a38f011adf834b2eef9297d395af6d
piotr.p <piotr.p@yggdrasilgaming.com>
2020-11-29 14:33:31 +0100

    Logo moved to the left edge

--- src/index.js
+++ src/index.js
@@ -8,19 +8,19 @@ document.body.appendChild(app.view);
 
 PIXI.Loader.shared.add('logo', logo).load((loader, resources) => {
 
-	const bunny = new PIXI.Sprite(resources.logo.texture);
+	const image = new PIXI.Sprite(resources.logo.texture);
 
-	bunny.x = app.renderer.width / 2;
-	bunny.y = app.renderer.height / 2;
+	image.x = 0;
+	image.y = app.renderer.height / 2;
 
-	bunny.anchor.x = 0.5;
-	bunny.anchor.y = 0.5;
+	image.anchor.x = 0.5;
+	image.anchor.y = 0.5;
 
-	app.stage.addChild(bunny);
+	app.stage.addChild(image);
 
 	const exactValueProvider = new ExactValueProvider();
 
 	app.ticker.add(() => {
-		bunny.rotation += exactValueProvider.giveMeOneHundredth();
+		image.rotation += exactValueProvider.giveMeOneHundredth();
 	});
 });
